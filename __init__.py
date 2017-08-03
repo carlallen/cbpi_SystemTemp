4999f428c1a98ffb329481551b84330084b20c73
Carl Allen <carl@allenofmn.com>
2017-08-03 17:45:09 -0500

    System Temp Sensor

---
+++ __init__.py
@@ -0,0 +1,57 @@
+import os, threading
+from modules import cbpi
+from modules.core.hardware import SensorPassive
+
+
+class myThread (threading.Thread):
+
+    value = 0
+
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.value = 0
+        self.running = True
+
+    def shutdown(self):
+        pass
+
+    def stop(self):
+        self.running = False
+
+    def run(self):
+
+        while self.running:
+            try:
+                app.logger.info("READ SYSTEM TEMP")
+                res = os.popen('vcgencmd measure_temp').readline()
+                temp = float(res.replace("temp=","").replace("'C\n",""))
+                self.value = temp
+            except:
+                pass
+
+            time.sleep(4)
+
+@cbpi.sensor
+class SystemTempSensor(SensorPassive):
+    def init(self):
+
+        self.t = myThread()
+
+        def shudown():
+            shudown.cb.shutdown()
+        shudown.cb = self.t
+
+        self.t.start()
+
+    def stop(self):
+        try:
+            self.t.stop()
+        except:
+            pass
+
+    def read(self):
+        if self.get_config_parameter("unit", "C") == "C":
+            self.data_received(round(self.t.value, 2))
+        else:
+            self.data_received(round(9.0 / 5.0 * self.t.value + 32, 2))
